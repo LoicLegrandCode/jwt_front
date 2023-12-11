9d6783509da912ec2eb098895f46fa8272f11161
Loïc <lolo62.grand@gmail.com>
2023-12-11 09:29:26 +0100

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import styles from "./Header.module.scss";
+import logo from "../../assets/images/logo.png";
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context";
+
+export default function Header() {
+  const {user, logout} = useContext(AuthContext);
+
+  return (
+    <header className={`d-flex align-items-center ${styles.header}`}>
+      <div className="flex-fill">
+        <NavLink end to="/">
+          <img src={logo} alt="logo" />
+        </NavLink>
+      </div>
+      <ul>
+        {!user ? (
+          <>
+            <NavLink to="login" className={`mr10 btn btn-primary-reverse`}>
+              <span>Login</span>
+            </NavLink>
+            <NavLink to="register" className={`mr10 btn btn-primary-reverse`}>
+              <span>Register</span>
+            </NavLink>
+          </>
+
+        ) : (
+          <>
+            <NavLink to="profile" className={`mr10 btn btn-primary-reverse`}>
+              <span>Profile</span>
+            </NavLink>
+            <NavLink className={`mr10 btn btn-primary-reverse`}>
+              <span onClick={logout}>Logout</span>
+            </NavLink>
+          </>
+        )}
+
+      </ul>
+    </header>
+  );
+}
